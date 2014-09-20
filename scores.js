c8a9d700ce4011aa318381a0b1bc65e08286733d
Graham McGregor <dev@grahammcgregor.ca>
2014-09-20 03:47:56 -0400

    Use bootstrap

--- scores.js
+++ scores.js
@@ -2,6 +2,23 @@ Router.map(function() {
   this.route('home', { path: '/' });
   this.route('login');
   this.route('game_selection');
-  this.route('play_game', { path: '/play_game/:_name' });
+  this.route('play_game', {
+    path: '/play_game/:_name',
+    data: function () {
+      var players = ['Graham', 'Carey', 'Michael'],
+        scoreItems = ['Settlements', 'Cities'];
+
+      return {
+        game: {
+          bgglink: 'http://boardgamegeek.com/boardgame/13/settlers-catan',
+          name: 'Settlers of Catan'
+        },
+        players: players,
+        scoreItems: scoreItems
+      };
+    }
+  });
   this.route('user_home');
 });
+
+
