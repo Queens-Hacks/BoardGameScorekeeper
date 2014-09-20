939e1b90af5c24c5b04530af431c4d9d6b4ea03e
Graham McGregor <dev@grahammcgregor.ca>
2014-09-20 08:11:27 -0400

    Make Search work

--- scores.js
+++ scores.js
@@ -6,7 +6,7 @@ Router.map(function() {
   this.route('game_search', {
     data: function() {
       return {
-        results: Games.find({ name: Session.get('query') })
+        results: Session.get('searchResults')
       };
     }
   });
@@ -31,8 +31,60 @@ Router.map(function() {
 if (Meteor.isClient) {
   Template.game_search.events({
     'submit .search-form': function (e, tmpl) {
-      Session.set('query', tmpl.find('input[name=gameName]').value);
       e.preventDefault();
+      Meteor.call('search', tmpl.find('input[name=gameName]').value, function (err, result){
+        if (err){
+          console.error("ERROR", err);
+        } else {
+          result = result.map(function (item){
+              item.thumbnail = item.thumbnail[0];
+              item.name = item.name[0].$.value;
+              return item;
+          });
+          Session.set('searchResults', result);
+          console.log(result);
+        }
+      });
+    }
+  });
+}
+
+var XML2JSCONFIG = {
+  explicitArray: true
+};
+if (Meteor.isServer) {
+  Meteor.methods({
+    search: function(query){
+      check(query, String);
+      var url = 'http://www.boardgamegeek.com/xmlapi2/search?query='+encodeURIComponent(query)+'&type=boardgame,boardgameexpansion';
+      var xml = HTTP.get(url).content;
+      var games;
+      xml2js.parseString(xml,
+        XML2JSCONFIG,
+        function (err, result){
+          if (err){
+            console.error("ERROR", err);
+          } else {
+            var gameIds = result.items.item.slice(0, Math.min(10, result.items.item.length)).map(
+              function (game){
+                return game.$.id;
+              }
+            );
+            var xml2 = HTTP.get('http://www.boardgamegeek.com/xmlapi2/thing?&id=' + gameIds.join()).content;
+            xml2js.parseString(xml2,
+              XML2JSCONFIG,
+              function (err, result){
+                if (err){
+                  console.error("ERROR", err);
+                } else {
+                  games = result.items.item;
+                }
+              }
+            );
+          }
+        }
+      );
+      return games;
     }
   });
 }
