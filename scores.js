78510ae807de78657a08ddb1dded323f1d391097
Graham McGregor <dev@grahammcgregor.ca>
2014-09-20 17:38:32 -0400

    Update user page

--- scores.js
+++ scores.js
@@ -212,6 +212,27 @@ if (Meteor.isClient) {
     });
   };
 
+UI.registerHelper('gt', function (c1, c2) {
+  return (c1 > c2);
+});
+UI.registerHelper('lt', function (c1, c2) {
+  return (c1 < c2);
+});
+UI.registerHelper('eq', function (c1, c2) {
+  return (c1 === c2);
+});
+UI.registerHelper('filterPlayerArray', function (array) {
+  var newArray = [];
+  array.forEach(function(player){
+    if (player.name){
+      newArray.push(player);
+    }
+  });
+
+  return newArray;
+});
+
+
 }
 
 var XML2JSCONFIG = {
