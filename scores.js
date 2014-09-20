e90f6dff614d6ab0c1fe872b8e6f210b4a536a5a
Graham McGregor <dev@grahammcgregor.ca>
2014-09-20 02:19:43 -0400

    Simple prototype

--- scores.js
+++ scores.js
@@ -1,3 +1,7 @@
 Router.map(function() {
   this.route('home', { path: '/' });
+  this.route('login');
+  this.route('game_selection');
+  this.route('play_game', { path: '/play_game/:_name' });
+  this.route('user_home');
 });
